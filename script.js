eae365f744d37e0d6ef7e086be01e370d2bee7c3
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-07-24 07:12:22 +0530

    fetching popUp changes

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 const searchBox = document.querySelector('.search-box')
 const searchBtn = document.querySelector('.search-btn')
 const recipeContainer = document.querySelector('.recipe-container')
+const recipePopContent = document.querySelector('.recipe-detail-content');
+const recipePopBtn = document.querySelector('.close-pop-btn')
 
 const fetchRecipe = async(item) => {
     recipeContainer.innerHTML = "<h2><i>Loading...</i></h2>"
@@ -19,12 +21,39 @@ const fetchRecipe = async(item) => {
         recipeDiv.innerHTML = `
             <img src="${element.strMealThumb}" alt="">
             <h3>${element.strMeal}</h3>
-            <p>${element.strArea}</p>
-            <p>${element.strCategory}</p>
+            <p><span>${element.strArea}</span> Dish</p>
+            <p><span>${element.strCategory}</span> Category</p>
         `
+        const button = document.createElement('button')
+        button.textContent = "View Recipe";
+
+        // Adding event linstener to button
+
+        button.addEventListener('click', () => {
+            openPopUp(element);
+        })
+
+
+
+        recipeDiv.appendChild(button);
+
         recipeContainer.appendChild(recipeDiv)
     });
 
+
+}
+
+const openPopUp = (elem) => {
+    recipePopContent.innerHTML = `
+        <h3>${elem.strMeal}</h3>
+        <p><span>Ingredients:</span></p>
+        <ul>${popIngredients(elem)}</ul>
+    `
+    recipePopContent.parentElement.style.display = 'block';  // its parentElement is "recipe-detail-pop"
+
+}
+// fetching ingredients
+const popIngredients = () => {
 
 }
 
